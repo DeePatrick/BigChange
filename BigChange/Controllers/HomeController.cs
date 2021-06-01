3ddbe5e955b3f89e751c8c81856df4e8f587b926
Chukwudozie Okudo <okudopato@yahoo.com>
2021-06-01 14:40:08 +0100

    Add project files.

---
+++ BigChange/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigChange.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            IEnumerable<int> Numbers = Enumerable.Range(1, 100)
+            .Where(num => num % 5 == 0 || num % 3 == 0)
+            .Take(50)
+            .Select(val => val);
+
+            ViewData.Model = Numbers;
+
+            return View(Numbers);
+        }
+
+    }
+}
---
+++ BigChange/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BigChange.MvcApplication" Language="C#" %>
